81bc31145d2c001802de75b1d69eeb5b66bfd515
Charlotte Lee <charlotteuxd@gmail.com>
2024-01-20 14:54:28 +0800

    Connect to API
    Connected to API to display the city + temperature according the search entry.

--- src/script.js
+++ src/script.js
@@ -1,9 +1,26 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let searchCity = document.querySelector("#city-name");
+
+  searchCity.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchTemperature(city) {
+  let apiKey = "ca80fb7d3o48t3c14460b13a3d83ca48";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let searchCity = document.querySelector("#city-name");
-  searchCity.innerHTML = searchInput.value;
+
+  searchTemperature(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchCity);
+
+searchTemperature("California");
